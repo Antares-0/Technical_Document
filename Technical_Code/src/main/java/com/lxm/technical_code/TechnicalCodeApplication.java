cb044278dd073de41aea812f4e5db6aaa384323b
liuxianming <liuxianming@58.com>
2024-07-24 15:23:23 +0800

    Init my project

---
+++ Technical_Code/src/main/java/com/lxm/technical_code/TechnicalCodeApplication.java
@@ -0,0 +1,13 @@
+package com.lxm.technical_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalCodeApplication.class, args);
+    }
+
+}
---
+++ Technical_Code/src/test/java/com/lxm/technical_code/TechnicalCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxm.technical_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

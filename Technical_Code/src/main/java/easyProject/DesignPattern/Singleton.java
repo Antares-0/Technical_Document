de4a7d5c83b7a256b80d66b53b79c9cb3fc513e7
liuxianming <liuxianming@58.com>
2024-11-16 17:33:00 +0800

    优化部分文件书写

--- Technical_Code/src/main/java/easyProject/DesignPattern/Singleton.java
+++ Technical_Code/src/main/java/easyProject/DesignPattern/Singleton.java
@@ -2,6 +2,11 @@ package easyProject.DesignPattern;
 
 public class Singleton {
 
+    // 为什么要使用volatile关键字？
+    // 如果这条命令出现了指令重排：instance = new Singleton();
+    // 指令重排过程中可能会出现对象初始化还没完毕就已经返回的情况
+    // 因此需要避免指令重排，而volatile可以保证这一点
+    // 因此在synchronized使用后，必须加上volatile
     private volatile static Singleton instance;
 
     private Singleton() {

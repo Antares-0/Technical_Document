889bfb910f7f0b811c36c86324caebf88e7b81fd
liuxianming <3016226031@tju.edu.cn>
2025-05-16 15:15:25 +0800

    feat(RAG):RAG高级检索技术

--- Technical_Code/src/main/java/com/lxm/technical_code/TestingCode.java
+++ Technical_Code/src/main/java/com/lxm/technical_code/TestingCode.java
@@ -1,19 +1,50 @@
 package com.lxm.technical_code;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: liuxianming
  * @Date: 2025/05/09 16:05:40
  */
 public class TestingCode {
-    public static void main(String[] args) {
-        ArrayList list = new ArrayList();
-        list.add("a");
-        list.add('a');
-        list.add(new TestingCode());
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> findZero = new ArrayList<>();
+        boolean isFindingZero = true;
+        for (int i = 0; i < words.length; i++) {
+            if (isFindingZero && groups[i] == 0) {
+                findZero.add(words[i]);
+                isFindingZero = false;
+                continue;
+            }
+            if (!isFindingZero && groups[i] == 1) {
+                findZero.add(words[i]);
+                isFindingZero = true;
+                continue;
+            }
         }
+
+        List<String> findOne = new ArrayList<>();
+        boolean isFindingOne = true;
+        for (int i = 0; i < words.length; i++) {
+            if (isFindingOne && groups[i] == 1) {
+                findOne.add(words[i]);
+                isFindingOne = false;
+                continue;
+            }
+            if (!isFindingOne && groups[i] == 0) {
+                findOne.add(words[i]);
+                isFindingOne = true;
+                continue;
+            }
+        }
+
+        return findOne.size() > findZero.size() ? findOne : findZero;
+
     }
+
+
+
+
+
 }

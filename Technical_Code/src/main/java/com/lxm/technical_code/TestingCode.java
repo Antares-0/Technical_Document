8fd9f98a1f79da225b89e04ab238231080ab1efc
liuxianming <3016226031@tju.edu.cn>
2025-10-04 16:12:22 +0800

    feat(文件更新):文件更新

--- Technical_Code/src/main/java/com/lxm/technical_code/TestingCode.java
+++ Technical_Code/src/main/java/com/lxm/technical_code/TestingCode.java
@@ -10,38 +10,11 @@ import java.util.Map;
  * @Date: 2025/05/09 16:05:40
  */
 public class TestingCode {
-    public List<String> getLongestSubsequence(String[] words, int[] groups) {
-        List<String> findZero = new ArrayList<>();
-        boolean isFindingZero = true;
-        for (int i = 0; i < words.length; i++) {
-            if (isFindingZero && groups[i] == 0) {
-                findZero.add(words[i]);
-                isFindingZero = false;
-                continue;
-            }
-            if (!isFindingZero && groups[i] == 1) {
-                findZero.add(words[i]);
-                isFindingZero = true;
-                continue;
-            }
-        }
 
-        List<String> findOne = new ArrayList<>();
-        boolean isFindingOne = true;
-        for (int i = 0; i < words.length; i++) {
-            if (isFindingOne && groups[i] == 1) {
-                findOne.add(words[i]);
-                isFindingOne = false;
-                continue;
-            }
-            if (!isFindingOne && groups[i] == 0) {
-                findOne.add(words[i]);
-                isFindingOne = true;
-                continue;
-            }
-        }
-
-        return findOne.size() > findZero.size() ? findOne : findZero;
-
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.version"));
+        Runtime.Version runtimeVersion = Runtime.version();
+        System.out.println(runtimeVersion.version());
     }
+
 }

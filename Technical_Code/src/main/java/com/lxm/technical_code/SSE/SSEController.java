78a475daebc381e230ad7ac8d5da7294b09000e2
liuxianming <3016226031@tju.edu.cn>
2025-10-28 18:48:17 +0800

    feat(sse):服务端推送

---
+++ Technical_Code/src/main/java/com/lxm/technical_code/SSE/SSEController.java
@@ -0,0 +1,75 @@
+package com.lxm.technical_code.SSE;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+
+@Controller
+@RequestMapping("/sse")
+@ResponseBody
+public class SSEController {
+
+//    // MediaType.TEXT_EVENT_STREAM_VALUE 用于表示sse推送返回结果
+//    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public SseEmitter Chat() throws Exception {
+//        SseEmitter emitter = new SseEmitter(0L);
+//
+//        CompletableFuture.runAsync(() -> {
+//            try {
+//                for (int i = 0; i < 10; i++) {
+//                    Date date = new Date();
+//                    Calendar calendar = Calendar.getInstance();
+//                    calendar.setTime(date);
+//                    int hour = calendar.get(Calendar.HOUR_OF_DAY); // 获取当前小时
+//                    int minute = calendar.get(Calendar.MINUTE); // 获取当前分钟
+//                    int second = calendar.get(Calendar.SECOND); // 获取当前秒钟
+//                    String time = "当前时间：" + hour + ":" + minute + ":" + second;
+//                    SseEmitter.SseEventBuilder event = SseEmitter.event();
+//                    event.data(time);
+//                    emitter.send(event);
+//                    Thread.sleep(1000);
+//                }
+//            } catch (Exception e) {
+//                // 异常退出
+//                emitter.completeWithError(e);
+//            } finally {
+//                emitter.complete();
+//            }
+//        });
+//        // 设置回调
+//        emitter.onCompletion(() -> System.out.println("SSE连接完成"));
+//        emitter.onTimeout(() -> System.out.println("SSE连接超时"));
+//        emitter.onError((ex) -> System.out.println("SSE连接错误: " + ex.getMessage()));
+//        return emitter;
+//    }
+
+    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter Chat() throws Exception {
+        SseEmitter emitter = new SseEmitter();
+
+        for (int i = 0; i < 10; i++) {
+            Date date = new Date();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY); // 获取当前小时
+            int minute = calendar.get(Calendar.MINUTE); // 获取当前分钟
+            int second = calendar.get(Calendar.SECOND); // 获取当前秒钟
+            String time = "当前时间：" + hour + ":" + minute + ":" + second;
+            SseEmitter.SseEventBuilder event = SseEmitter.event();
+            event.data(time);
+            emitter.send(event);
+            Thread.sleep(1000);
+        }
+        emitter.onTimeout(emitter::complete);
+        return emitter;
+    }
+
+}

d4550a29a74ca1e1bdb655e311a094b44234d232
liuxianming <3016226031@tju.edu.cn>
2026-04-23 10:01:24 +0800

    feat(RL):策略梯度法更新暂存

---
+++ LLM/ReinforcementLearning/code&data/chap02/PolicyGradient/cart.py
@@ -0,0 +1,49 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+class Policy(nn.Module):
+    def __init__(self, action_size=2):
+        super(Policy, self).__init__()
+        self.layer1 = nn.Linear(4, 512)
+        self.layer2 = nn.Linear(512, action_size)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.softmax(self.layer2(x), dim=-1)
+        return x
+
+class Agent:
+    def __init__(self):
+        self.gamma = 0.99
+        self.lr = 0.0002
+        self.action_size = 2
+
+        self.memory = []
+        self.pi = Policy(self.action_size)
+        self.optimizer = optim.Adam(self.pi.parameters(), lr=self.lr)
+
+    def get_action(self, state):
+        state = torch.tensor(state, dtype=torch.float32)
+        probs = self.pi.forward(state) # 求的是概率分布
+        m = Categorical(probs)
+        action = m.sample().item()
+        return action, probs[action]
+
+    def add(self, reward, prob):
+        data = (reward, prob)
+        self.memory.append(data)
+
+    def update(self):
+        G, loss = 0,0
+        for reward, prob in reversed(self.memory):
+            G = reward + self.gamma * G
+            loss += -torch.log(prob) * G
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()  # 必须加这一步更新参数！
+        self.memory = []
